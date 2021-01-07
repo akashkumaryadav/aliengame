573221d060b919bcdc56edf82e09aa9e939a7363
akashkumaryadav <akash.kumar.yadav.cse@gmail.com>
2021-01-08 00:07:38 +0530

    AlienZombie decision making desktop game

---
+++ src/components/GamestateEnd.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="modal">
+    <h1 :style="{ color: status }">
+      {{ uiState }}
+    </h1>
+    <h1>you score is {{ score }}</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["score", "uiState"])
+  },
+  props: ["status"]
+};
+</script>
